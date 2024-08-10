93ba9bd47a01ccd224c796bfba9dc149aa3decc2
datmedevil17 <rakshitshukla17@gmail.com>
2024-08-11 03:25:11 +0530

    adding light and shadows

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,49 @@
 import './App.css';
 import { Canvas } from "@react-three/fiber";
 import Sphere from './components/Sphere';
-import { OrbitControls,useTexture } from '@react-three/drei';
+import { OrbitControls, useHelper } from '@react-three/drei';
+import { useRef } from 'react';
+import { DirectionalLightHelper } from 'three';
+
+function DirectionalLightWithHelper() {
+  const lightRef = useRef(null);
+
+  useHelper(lightRef, DirectionalLightHelper, 5, "red");
+
+  return (
+    <directionalLight
+      ref={lightRef}
+      color="white"
+      position={[0, 10, 10]}
+      castShadow
+      shadow-mapSize-width={1024}
+      shadow-mapSize-height={1024}
+      shadow-camera-far={50}
+      shadow-camera-near={0.1}
+      shadow-camera-left={-10}
+      shadow-camera-right={10}
+      shadow-camera-top={10}
+      shadow-camera-bottom={-10}
+    />
+  );
+}
 
 function App() {
-
-  
   return (
     <div className="container">
-      <Canvas camera={{ position: [0, 0, 10], fov: 50 }}>
-        <OrbitControls/>
+      <Canvas camera={{ position: [0, 5, 10], fov: 50 }} shadows>
+        <OrbitControls />
         <ambientLight intensity={0.3} />
-        <directionalLight color="white" position={[5, 5, 5]} />
+        <DirectionalLightWithHelper />
         <Sphere />
+        <mesh
+          rotation={[-Math.PI / 2, 0, 0]}
+          position={[0, -2, 0]}
+          receiveShadow
+        >
+          <planeGeometry args={[1000, 1000]} />
+          <meshStandardMaterial color={"#458745"} />
+        </mesh>
       </Canvas>
     </div>
   );
--- src/components/Sphere.jsx
+++ src/components/Sphere.jsx
@@ -1,8 +1,10 @@
 import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
-import { useTexture } from "@react-three/drei";
+import { useHelper, useTexture } from "@react-three/drei";
+import { DirectionalLightHelper } from "three";
 const Sphere = () => {
   const meshRef = useRef(null);
+  
 
   const map = useTexture("./textures/cracked_concrete_diff_4k.png")
   const displacementMap = useTexture("./textures/cracked_concrete_disp_4k.png")
@@ -17,18 +19,12 @@ const Sphere = () => {
 
   return (
     <>
-      <mesh ref={meshRef} scale={[1, 1, 1]} position={[-3, 0, 0]} >
+     
+      <mesh scale={[1, 1, 1]} position={[0, 2, 0]} castShadow>
         <sphereGeometry />
         <meshStandardMaterial   map={map} normalMap={normalMap} displacementMap={displacementMap} roughnessMap={roughnessMap}/>
       </mesh>
-      <mesh scale={[1, 1, 1]} position={[0, 0, 0]}>
-        <sphereGeometry />
-        <meshStandardMaterial   map={map} normalMap={normalMap} displacementMap={displacementMap} roughnessMap={roughnessMap}/>
-      </mesh>
-      <mesh scale={[1, 1, 1]} position={[3, 0, 0]}>
-        <sphereGeometry />
-        <meshStandardMaterial   map={map} normalMap={normalMap} displacementMap={displacementMap} roughnessMap={roughnessMap}/>
-      </mesh>
+     
     </>
   );
 };
